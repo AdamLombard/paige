bc692911bd3ca3b6d543ef999e26db4f58212f10
Adam Lombard <willcodeforgin@gmail.com>
2017-10-22 11:34:19 -0700

    move weather case into function

--- paige.js
+++ paige.js
@@ -5,7 +5,7 @@ var test = function(context, cb, value) {
   cb(null, { text: value } );
 }
 
-var joke = function(context, cb, data) {
+var joke = function(context, cb) {
   var result ='';
   var returnMsg = '';
   var jokes = [
@@ -88,93 +88,98 @@ var joke = function(context, cb, data) {
   }
 }
 
-module.exports = function(context, cb) {
-  var command = context.body.text.split(' ')[1];
+var weather = function(context, cb) {
+  console.log("in the function now!");
   var result = '';
   var returnMsg = '';
+  var location = context.body.text.split(' ')[2];
+  var weatherAPI = context.secrets.weatherURL
+            + 'forecast?'
+            + 'zip=' + location
+            + '&appid=' + context.secrets.weatherToken;
+
+  request.get(weatherAPI, function (error, res, body) {
+    if (error) {
+      console.log(error);
+      cb(null, {text: "Hmm. That didn't work..."} );
+    } else {
+      var result = JSON.parse(body);
+
+      var icons = {
+        "d": ":cityscape:",
+        "n": ":night_with_stars:",
+        
+        // clear sky
+        "01d": ":sunny:",
+        "01n": ":night_with_stars:",
+        
+        // few clouds
+        "02d": ":partly_sunny:",
+        "02n": ":cloud:",
+        
+        // scattered clouds
+        "03d": ":barely_sunny:",
+        "03n": ":cloud:",
+        
+        // broken clouds
+        "04d": ":cloud:",
+        "04n": ":cloud:",
+        
+        // shower rain
+        "09d": ":partly_sunny_rain:",
+        "09n": ":rain_cloud:",
+        
+        // rain
+        "10d": ":rain_cloud:",
+        "10n": ":rain_cloud:",
+        
+        // thunderstorm
+        "11d": ":lightning:",
+        "11n": ":lightning:",
+        
+        // snow
+        "13d": ":snowflake:",
+        "13n": ":snowflake:",
+        
+        // mist
+        "50d": ":fog:",
+        "50n": ":fog:"
+      };
+
+      var forecasts = result.list;
+      returnMsg = "Here's the weather for the next 24 hours in " + result.city.name + "!";
+      for (var i = 0; i <= 8; i ++) {
+        var forecast = forecasts[i];
+        var nightOrDay = forecast.weather[0].icon.split("")[2];
+        var weatherIconCode = forecast.weather[0].icon;
+        var cTemp = (forecast.main.temp - 273).toFixed(0);
+        var fTemp = ((1.8 * cTemp) + 32).toFixed(0);
+        var dayAndTime = moment.tz(forecast.dt * 1000, 'America/Los_Angeles').format('ddd h:mm a');
+        var weatherDescription = forecast.weather[0].description;
+
+        returnMsg += "\n\t";
+        returnMsg += dayAndTime;
+        // returnMsg += " - " + icons[nightOrDay];
+        returnMsg += "\n\t\t";
+        returnMsg += icons[weatherIconCode] + "\t";
+        returnMsg += cTemp + "°C (" + fTemp +"°F) with " + weatherDescription;
+      }
+
+      cb(null, { text: returnMsg } );
+    }
+  });
+}
+
+module.exports = function(context, cb) {
+  var command = context.body.text.split(' ')[1];
 
   switch (command) {
     case 'test':
       test(context, cb, "hello")
       break;
 
-      case 'weather':
-      var location = context.body.text.split(' ')[2];
-      var weatherAPI = context.secrets.weatherURL
-                + 'forecast?'
-                + 'zip=' + location
-                + '&appid=' + context.secrets.weatherToken;
-
-      request.get(weatherAPI, function (error, res, body) {
-        if (error) {
-          console.log(error);
-          cb(null, {text: "Hmm. That didn't work..."} );
-        } else {
-          var result = JSON.parse(body);
-
-          var icons = {
-            "d": ":cityscape:",
-            "n": ":night_with_stars:",
-            
-            // clear sky
-            "01d": ":sunny:",
-            "01n": ":night_with_stars:",
-            
-            // few clouds
-            "02d": ":partly_sunny:",
-            "02n": ":cloud:",
-            
-            // scattered clouds
-            "03d": ":barely_sunny:",
-            "03n": ":cloud:",
-            
-            // broken clouds
-            "04d": ":cloud:",
-            "04n": ":cloud:",
-            
-            // shower rain
-            "09d": ":partly_sunny_rain:",
-            "09n": ":rain_cloud:",
-            
-            // rain
-            "10d": ":rain_cloud:",
-            "10n": ":rain_cloud:",
-            
-            // thunderstorm
-            "11d": ":lightning:",
-            "11n": ":lightning:",
-            
-            // snow
-            "13d": ":snowflake:",
-            "13n": ":snowflake:",
-            
-            // mist
-            "50d": ":fog:",
-            "50n": ":fog:"
-          };
-
-          var forecasts = result.list;
-          returnMsg = "Here's the weather for the next 24 hours in " + result.city.name + "!";
-          for (var i = 0; i <= 8; i ++) {
-            var forecast = forecasts[i];
-            var nightOrDay = forecast.weather[0].icon.split("")[2];
-            var weatherIconCode = forecast.weather[0].icon;
-            var cTemp = (forecast.main.temp - 273).toFixed(0);
-            var fTemp = ((1.8 * cTemp) + 32).toFixed(0);
-            var dayAndTime = moment.tz(forecast.dt * 1000, 'America/Los_Angeles').format('ddd h:mm a');
-            var weatherDescription = forecast.weather[0].description;
-
-            returnMsg += "\n\t";
-            returnMsg += dayAndTime;
-            // returnMsg += " - " + icons[nightOrDay];
-            returnMsg += "\n\t\t";
-            returnMsg += icons[weatherIconCode] + "\t";
-            returnMsg += cTemp + "°C (" + fTemp +"°F) with " + weatherDescription;
-          }
-
-          cb(null, { text: returnMsg } );
-        }
-      });
+    case 'weather':
+      weather(context, cb)
       break;
 
     case 'joke':
